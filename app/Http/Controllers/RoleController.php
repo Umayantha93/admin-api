6a0b19f6d7c1941176a805cfdc3b0e5c53326645
Umayantha93 <umayantha93@gmail.com>
2024-05-26 18:08:48 +0530

    implement resources and controllers

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -12,7 +12,7 @@ class RoleController extends Controller
 
     public function index()
     {
-        return RoleResource::collection(Role::all());
+        return RoleResource::collection(Role::paginate());
     }
 
     public function store(Request $request)
@@ -27,7 +27,7 @@ class RoleController extends Controller
      */
     public function show(string $id)
     {
-        return new RoleResource(Role::find($id));
+        return new RoleResource(Role::with('permissions')->find($id));
     }
 
     public function update(Request $request, string $id)
