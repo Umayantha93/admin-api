e550e2d854453db2537d722ccf00735ab3a9bc66
Umayantha93 <umayantha93@gmail.com>
2024-05-26 09:43:01 +0530

    generate migration on roles and users create foreign keys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,8 +14,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Role::factory()->create([
+            'name' => 'Admin'
+        ]);
+
+        Role::factory()->create([
+            'name' => 'Editor'
+        ]);
+
+        Role::factory()->create([
+            'name' => 'Viewer'
+        ]);
 
         User::factory(20)->create();
+
+        User::factory()->create([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'role_id' => 1,
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Editor',
+            'last_name' => 'Editor',
+            'email' => 'editor@editor.com',
+            'role_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Viewer',
+            'last_name' => 'Viewer',
+            'email' => 'viewer@viewer.com',
+            'role_id' => 3,
+        ]);
     }
 }
